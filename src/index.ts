bda4ad65c2ff162ca4397fb1da66b01f58d4652e
Elliot Plant <elliotaplant@gmail.com>
2024-11-01 16:20:03 -0700

    Get things started

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,59 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import {
+  R2Bucket,
+  ExecutionContext,
+} from '@cloudflare/workers-types';
+
+interface Env {
+  ESPN_FANTASY_PHOTOS_BUCKET: R2Bucket;
+}
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const url = new URL(request.url);
+    const key: string = url.pathname.slice(1);
+
+		if (!key) {
+			// return HTML page
+		}
+
+    switch (request.method) {
+      case 'PUT': {
+        await env.ESPN_FANTASY_PHOTOS_BUCKET.put(key, request.body);
+        return new Response(`Put ${key} successfully!`);
+      }
+
+      case 'GET': {
+        const object = await env.ESPN_FANTASY_PHOTOS_BUCKET.get(key);
+
+        if (object === null) {
+          return new Response('Object Not Found', { status: 404 });
+        }
+
+        const headers = new Headers();
+        object.writeHttpMetadata(headers);
+        headers.set('etag', object.httpEtag);
+
+        return new Response(object.body, {
+          headers,
+        });
+      }
+
+      case 'DELETE': {
+        await env.ESPN_FANTASY_PHOTOS_BUCKET.delete(key);
+        return new Response('Deleted!');
+      }
+
+      default:
+        return new Response('Method Not Allowed', {
+          status: 405,
+          headers: {
+            Allow: 'PUT, GET, DELETE',
+          },
+        });
+    }
+  },
 } satisfies ExportedHandler<Env>;
